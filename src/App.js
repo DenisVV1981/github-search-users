48d7a5d7801be54a872b6a94563628120fd613ba
Denis Vasilev <d89615022404@gmail.com>
2024-04-29 20:43:51 +0200

    small fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Searchbar } from './components/Searchbar/Searchbar';
+import Searchbar from './components/Searchbar/Searchbar.jsx';
 
 
 function App() {
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as S from './Searchbar.styles'
 
-export function Searchbar() {
+export default function Searchbar() {
   return <S.SearchInput/>
 }
