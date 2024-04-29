8400727e737068aaa77e0307aebea9fdb0eda8ec
Denis Vasilev <d89615022404@gmail.com>
2024-04-29 22:47:16 +0200

    Создал черновик интерфейса

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-import Searchbar from './components/Searchbar/Searchbar.jsx';
-
+import SearchContainer from './components/SearchContainer/SearchContainer.jsx';
+import SelectedUser from './components/SelectedUser/SelectedUser.jsx';
 
 function App() {
+  const [selectedUser, setSelectedUser] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Searchbar></Searchbar>
+      <SearchContainer
+        selectUserCb={setSelectedUser}/>
+      <SelectedUser 
+        user={selectedUser}/>
     </div>
   );
 }
--- src/components/Searchbar/Searchbar.styles.js
+++ src/components/Searchbar/Searchbar.styles.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
-export const SearchInput= styled.input`
-
+export const SearchbarContainer= styled.div`
+border: 1px solid red;
+background: light-grey;
+`
+export const SearchbarInput= styled.input`
+background: light-grey;
 `
